03d5152c340e63789cf902dc4de1012cfc9a5b15
Sofiamishel2003 <sofiamishel2003@gmail.com>
2026-02-12 17:45:10 -0600

    Feat: Commit Inicial Stream Cypher Normal

---
+++ Ejercicio_streamcypher.py
@@ -0,0 +1,35 @@
+import random
+from random import randint
+
+# Keystream Aleatorio
+def keystream_rand(size):
+    key = []
+    for i in range(size):
+        key.append(random.randint(0, 255))
+    return key
+def encrypt(plaintext, keystream):
+    ciphertext = []
+    for i in range(len(plaintext)):
+        ciphertext.append(plaintext[i] ^ keystream[i])
+    return bytes(ciphertext)
+def decrypt(ciphertext, keystream):
+    plaintext = []
+    for i in range(len(ciphertext)):
+        plaintext.append(ciphertext[i] ^ keystream[i])
+    return bytes(plaintext)
+
+texto = "Hola Mundo"
+# Convertir el texto a binario
+binario = texto.encode('utf-8')
+# Generar un keystream aleatorio del mismo tamaño que el texto
+keystream = keystream_rand(len(binario))
+keystream_bytes = bytes(keystream)
+# Encriptar el texto
+ciphertext = encrypt(binario, keystream)
+print(f"Texto original: {texto}")
+print(f"Texto en binario: {binario}")
+print(f"Keystream: {keystream_bytes}")
+print(f"Texto encriptado: {ciphertext}")
+# Desencriptar el texto
+decrypted_text = decrypt(ciphertext, keystream) 
+print(f"Texto desencriptado: {decrypted_text.decode('utf-8')}")
